265f4cf6c03273d6425e6eaeefe4a57397ea3346
Krishna240895 <krishnamoorthy880@gmail.com>
2025-04-04 22:56:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth Scrolling for Internal Anchor Links
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+});
